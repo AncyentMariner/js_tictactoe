411ee412b6a6c44ad6ea9a8f877240dd214862f7
ancyentMariner <jeffreyckingswood@gmail.com>
2016-12-23 21:58:15 -0500

    working on stlying and setting up reset board function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //make game board
 const content = document.getElementById('game');
-const boardSize = 3;
+const scoreBoard = document.querySelector('#score');
 
 function makeBoard() {
   const table = document.createElement('table');
@@ -8,9 +8,9 @@ function makeBoard() {
   table.id = "board";
 
   let counter = 0;
-  for (let i = 0; i < boardSize;i++) {
+  for (let i = 0; i < 3; i++) {
     let row = tbody.insertRow();
-    for (let j = 0; j < boardSize;j++) {
+    for (let j = 0; j < 3; j++) {
       let td = row.insertCell();
       td.id = (counter++);
       td.classList.add('cell');
@@ -23,7 +23,6 @@ function makeBoard() {
 makeBoard();
 
 //mark player moves and check for winner
-const gameBoard = document.querySelector('#board');
 const playerOne = 'X';
 const playerTwo = 'O';
 let currentTurn = playerOne;
@@ -34,28 +33,23 @@ function markGameMove(e) {
     e.target.innerHTML = currentTurn;
     e.target.value = currentTurn;
     e.target.classList.add('marked');
+    checkForWinner(this);
     currentTurn = currentTurn === playerOne ? playerTwo : playerOne;
     moves += 1;
-    checkForWinner(this);
   } else {
     alert('this space is taken');
   }
 }
 
-function checkForWinner(clicked) {
-  let squares = Array.from(document.querySelectorAll('.marked'));
-  // let markedSquares = squares.filter(filterSquares);
+function reset() {
+  const gameBoard = document.querySelector('#board');
+  gameBoard.outerHTML = '';
+  makeBoard();
+}
 
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+function checkForWinner() {
+  let squares = Array.from(document.querySelectorAll('.marked'));
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
   //wait until first player has moved three times to check
   if (squares.length > 4) {
@@ -66,20 +60,14 @@ function checkForWinner(clicked) {
       let id_c = document.getElementById(c);
 
       if (id_a.value && id_a.value === id_b.value && id_a.value === id_c.value) {
-        console.log('works?');
+        scoreBoard.innerHTML = `${currentTurn} wins!`;
         return id_a;
+      }
+
+      if (moves === 9) {
+        scoreBoard.innerHTML = 'The game was a draw';
       }
     }
     return null;
   }
 }
-//
-// function filterSquares(elem) {
-//   if (elem.classList.contains('X')) {
-//     elem.value = 'x';
-//     return elem;
-//   } else if (elem.classList.contains('O')) {
-//     elem.value = 'o';
-//     return elem;
-//   }
-// }
